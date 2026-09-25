1e2815b02e83f1dc4bf4b222dafa665d0702b2a1
Dio_Exodi <exodosius@gmail.com>
2026-09-25 09:30:46 +0000

    Retarget stack transition test to surviving pages

--- androidApp/src/test/kotlin/com/exodidio/tune/StackPageTransitionTest.kt
+++ androidApp/src/test/kotlin/com/exodidio/tune/StackPageTransitionTest.kt
@@ -10,21 +10,21 @@ class StackPageTransitionTest {
     @Test
     fun pushesInStackAreIdentifiedAsForwardTransitions() {
         val rootKey = PageKey(AppDestination.Settings, AppStackPage.Root, index = 0)
-        val syncKey = PageKey(AppDestination.Settings, AppStackPage.SettingsSync, index = 1)
-        val scannerKey = PageKey(AppDestination.Settings, AppStackPage.SettingsSyncScanner, index = 2)
+        val appearanceKey = PageKey(AppDestination.Settings, AppStackPage.SettingsAppearance, index = 1)
+        val playbackKey = PageKey(AppDestination.Settings, AppStackPage.SettingsPlayback, index = 2)
 
-        assertTrue(isForwardTransition(target = syncKey, initial = rootKey))
-        assertTrue(isForwardTransition(target = scannerKey, initial = syncKey))
+        assertTrue(isForwardTransition(target = appearanceKey, initial = rootKey))
+        assertTrue(isForwardTransition(target = playbackKey, initial = appearanceKey))
     }
 
     @Test
     fun popsInStackAreIdentifiedAsBackwardTransitions() {
         val rootKey = PageKey(AppDestination.Settings, AppStackPage.Root, index = 0)
-        val syncKey = PageKey(AppDestination.Settings, AppStackPage.SettingsSync, index = 1)
-        val scannerKey = PageKey(AppDestination.Settings, AppStackPage.SettingsSyncScanner, index = 2)
+        val appearanceKey = PageKey(AppDestination.Settings, AppStackPage.SettingsAppearance, index = 1)
+        val playbackKey = PageKey(AppDestination.Settings, AppStackPage.SettingsPlayback, index = 2)
 
-        assertFalse(isForwardTransition(target = syncKey, initial = scannerKey))
-        assertFalse(isForwardTransition(target = rootKey, initial = syncKey))
+        assertFalse(isForwardTransition(target = appearanceKey, initial = playbackKey))
+        assertFalse(isForwardTransition(target = rootKey, initial = appearanceKey))
     }
 
     @Test
