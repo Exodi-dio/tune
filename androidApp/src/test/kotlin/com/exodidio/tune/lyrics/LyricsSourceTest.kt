ebd5582347551a3cdee5d499a8e91bd191cc3c35
Dio_Exodi <exodosius@gmail.com>
2026-09-26 07:14:06 +0000

    tmp: task-4 lyrics grep gate (RED probe)

--- androidApp/src/test/kotlin/com/exodidio/tune/lyrics/LyricsSourceTest.kt
+++ androidApp/src/test/kotlin/com/exodidio/tune/lyrics/LyricsSourceTest.kt
@@ -1,14 +1,21 @@
 package com.exodidio.tune.lyrics
 
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
 import org.junit.Test
 
 class LyricsSourceTest {
     @Test
-    fun selectsConfiguredSourceAndFallsBackToTheOther() {
-        assertEquals("desktop", preferredLyrics(LyricsSource.Desktop, "desktop", "provider"))
-        assertEquals("provider", preferredLyrics(LyricsSource.AutoFetch, "desktop", "provider"))
-        assertEquals("desktop", preferredLyrics(LyricsSource.AutoFetch, "desktop", null))
-        assertEquals("provider", preferredLyrics(LyricsSource.Desktop, null, "provider"))
+    fun defaultSourceIsAutoFetch() {
+        assertEquals(LyricsSource.AutoFetch, LyricsSettings().preferredSource)
+        assertEquals(LyricsSource.AutoFetch, LyricsSource.fromStorage(null))
+        assertEquals(LyricsSource.AutoFetch, LyricsSource.fromStorage("desktop"))
+        assertEquals(LyricsSource.AutoFetch, LyricsSource.fromStorage("auto_fetch"))
+    }
+
+    @Test
+    fun preferredLyricsReturnsProviderLyrics() {
+        assertEquals("provider", preferredLyrics("provider"))
+        assertNull(preferredLyrics(null))
     }
 }
